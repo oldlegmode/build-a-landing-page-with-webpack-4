f6d76a2bca5c8904b99740d6a64dc9a93867b581
Oleg Filkin <ya.filkinoleg@yandex.com>
2019-05-22 22:03:53 +0300

    master: fixed dependences

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
